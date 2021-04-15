7139a23e001ec2c59e79b840c0412ad0254117b9
Livan <livanar00@gmail.com>
2021-04-15 11:02:27 -0400

    Otra prueba

--- Asiel/settings.py
+++ Asiel/settings.py
@@ -90,9 +90,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'Asiel', 'static'),
-)
+
 SITE_ID = 2
 
 
