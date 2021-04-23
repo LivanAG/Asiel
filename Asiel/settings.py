e8e2bf758ad5778268281e70268483bd8b531237
Livan <livanar00@gmail.com>
2021-04-23 14:07:56 -0400

    conf nueva 2.0

--- Asiel/settings.py
+++ Asiel/settings.py
@@ -225,7 +225,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
 #DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 
 dbx = dropbox.Dropbox (DROPBOX_OAUTH2_TOKEN)
-DROPBOX_ROOT_PATH = '/Aplicaciones/Asiel Blog/'
+DROPBOX_ROOT_PATH = '/media/'
 #print(dbx.users_get_current_account())
 #print(dbx.users_get_space_usage())
 
@@ -414,8 +414,8 @@ FILER_STORAGES = {
                 #'location': MEDIA_ROOT,
                 #'base_url': MEDIA_URL,
             },
-            #'UPLOAD_TO': 'filer.utils.generate_filename.randomized',
-            #'UPLOAD_TO_PREFIX': 'filer_public',
+            'UPLOAD_TO': 'filer.utils.generate_filename.randomized',
+            'UPLOAD_TO_PREFIX': 'filer_public',
         },
         'thumbnails': {
             'ENGINE': DEFAULT_FILE_STORAGE,
