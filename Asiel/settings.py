d04b20a23ad788f78d8a8b88017adcce2a28bac5
Livan <livanar00@gmail.com>
2021-04-27 13:37:48 -0400

    Cabiando el dom

--- Asiel/settings.py
+++ Asiel/settings.py
@@ -320,7 +320,7 @@ EMAIL_USE_TLS=True
 EMAIL_PORT = 587
 EMAIL_HOST_USER="frankaranda1960@gmail.com"
 EMAIL_HOST_PASSWORD="aranda@1960"
-DOMAIN = 'https://shuman-blog.herokuapp.com/'
+DOMAIN = 'shuman-blog.herokuapp.com'
 
 #------------------ END Configuracion Email--------------------------------------------------------------------------------------
 
