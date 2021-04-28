e46a56325aa76ee8fb869821fc24e153c180c46e
Livan <livanar00@gmail.com>
2021-04-28 13:23:00 -0400

    Usando Contrasegna de app

--- Asiel/settings.py
+++ Asiel/settings.py
@@ -321,8 +321,8 @@ EMAIL_HOST="smtp.gmail.com"
 EMAIL_USE_TLS=True
 EMAIL_PORT = 587
 EMAIL_HOST_USER="frankaranda1960@gmail.com"
-EMAIL_HOST_PASSWORD="aranda@1960"
-#DOMAIN = 'shuman-blog.herokuapp.com'
+EMAIL_HOST_PASSWORD="rwlqymgxsddnjnfa"
+
 
 #------------------ END Configuracion Email--------------------------------------------------------------------------------------
 
