2d2661c55d2822daf4412194d300b74d7ee4d681
LivanAG <79125230+LivanAG@users.noreply.github.com>
2022-06-26 21:38:21 -0400

    Update settings.py

--- Asiel/settings.py
+++ Asiel/settings.py
@@ -320,8 +320,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_USE_TLS=True
 EMAIL_PORT = 587
-EMAIL_HOST_USER="frankaranda1960@gmail.com"
-EMAIL_HOST_PASSWORD="rwlqymgxsddnjnfa"
+EMAIL_HOST_USER="marcelosking00@gmail.com"
+EMAIL_HOST_PASSWORD="zgbzmpcpinhoqsvn"
 
 
 #------------------ END Configuracion Email--------------------------------------------------------------------------------------
